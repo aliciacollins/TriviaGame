473f0593bcc52ebf1f88bcedf06d779117ce6ec4
Alicia Collins <alicia.collins60@me.com>
2018-07-08 17:58:02 -0700

    update3

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,16 @@
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
- var answers = ["D","B","A","C","A","A","D","B"], 
+    $( "#set2" ).slideUp( 300 ).hide( 1800 );
+    $( "#set1" ).fadeIn( 1800 );
+    $("#submit").hide();
+    
+    var answers = ["D","B","A","C","A","A","D","B"], 
 tot = answers.length;
 
 function getCheckedValue( radioName ){
 var radios = document.getElementsByName( radioName );
 console.log("radioName",radioName); // Get radio group by-name
 for(var y=0; y<radios.length; y++)
-  if(radios[y].checked) return radios[y].value; // return the checked value
+    if(radios[y].checked) return radios[y].value; // return the checked value
 
 }
 
@@ -20,20 +22,47 @@ return score;
 }
 
 function returnScore(){
-alert("Your score is "+ getScore() +"/"+ tot);
+$("#score").text("You have completed the quiz you scored " + getScore() + " out of 8 points");
+console.log("get score",getScore());
+$("#set1").hide();
+$("#set2").hide();
+$("#score").show();
 }   
-    $(document).ready(function () {
-        $("#hide").click(function () {
-            $("#set1").hide();
-        });
-        $("#hide2").click(function () {
-            $("#set2").hide();
-        });
-        $("#show").click(function () {
-            $("#set1").show();
-        });
-        $("#show2").click(function () {
-            $("#set2").show();
-        });
+$(document).ready(function () {
+    $("#hide").click(function () {
+        $("#set1").hide();
+        $("#set2").show();
+        $("#hide").hide();
+        $("#submit").show();
     });
+
+    
+});
+
+
+
+var time_in_minutes = 3;
+var current_time = Date.parse(new Date());
+var deadline = new Date(current_time + time_in_minutes*60*1000);
+
+
+function time_remaining(endtime){
+	var t = Date.parse(endtime) - Date.parse(new Date());
+	var seconds = Math.floor( (t/1000) % 60 );
+	var minutes = Math.floor( (t/1000/60) % 60 );
+
+	return {'total':t,'minutes':minutes, 'seconds':seconds};
+}
+function run_clock(id,endtime){
+	var clock = document.getElementById(id);
+	function update_clock(){
+		var t = time_remaining(endtime);
+		clock.innerHTML = 'minutes: '+t.minutes+'<br>seconds: '+t.seconds;
+		if(t.total<=0){ clearInterval(timeinterval); }
+	}
+	update_clock(); // run function once at first to avoid delay
+	var timeinterval = setInterval(update_clock,1000);
+}
+run_clock('clockdiv',deadline);
+ 
 
